47f6ac7a2b47781ba3a848bd9df69724e2fc5e19
sokthearymao <68365381+sokthearymao@users.noreply.github.com>
2020-07-29 13:37:26 -0600

    update

--- src/App.js
+++ src/App.js
@@ -8,13 +8,18 @@ import Message from './components/Message';
 import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <FunctionClick />
-        <ClassClick />
+        <ParentComponent />
+        {/* <EventBind /> */}
+        {/* <FunctionClick />
+        <ClassClick /> */}
         {/* <Counter />
         <Message />
         props example
